b57e401f37fa2936465d1d6b3bee7860d415e415
Thomas Samuel <fresher-unnamed-0b@icloud.com>
2024-11-29 11:16:23 +0000

    Uploading Web Bits

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Initialize Materialize components
+document.addEventListener('DOMContentLoaded', function () {
+  const collapsibles = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(collapsibles);
+});
